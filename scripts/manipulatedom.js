03fcac976c0d32f46addcbae37a6d17208b4249c
VictorZhang96 <zhangxucheng01@163.com>
2024-02-06 18:01:21 -0500

    020524 v1

--- scripts/manipulatedom.js
+++ scripts/manipulatedom.js
@@ -2,6 +2,10 @@ window.onload = function() {
   document.getElementById('addTableBtn').addEventListener('click', addTable);
 }
 
+window.onload = function() {
+  document.getElementById('addTableBtn').addEventListener('click', editTable);
+}
+
 function createTRNode(colNodes) {
   let trNode = document.createElement("tr");
   colNodes.forEach(function(colNode) {
@@ -22,10 +26,20 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
-function addTable() {
+function removeTable() {
+  const root = document.getElementById("root");
+  const existingTable = root.querySelector("table");
+  if (existingTable) {
+    root.removeChild(existingTable);
+  }
+}
+
+function editTable() {
+  removeTable();
+
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+    let col1 = createTDNode(createTxtNode("New Cell (" + i + ", 0)"));
     tableNode.appendChild(createTRNode([col1]));
   }
   document.getElementById("root").appendChild(tableNode);
